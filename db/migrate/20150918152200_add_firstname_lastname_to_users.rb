03cffbdf401efb699fb24d8576465894cf6a1c66
morihito <bartek.lesniak86@gmail.com>
2015-09-18 15:32:38 +0000

    Fixed user model
    New migration with firstname, lastname and admin fields.
    Admin field set to be false by default and null.
    Added firstname and lastname presence validation.

---
+++ db/migrate/20150918152200_add_firstname_lastname_to_users.rb
@@ -0,0 +1,7 @@
+class AddFirstnameLastnameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :admin, :boolean, default: false, null: false
+  end
+end
