e882118509647d196588b1159aa5397eed0495d0
morihito <bartek.lesniak86@gmail.com>
2015-09-18 15:53:14 +0000

    Added routes for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+    #set strong parameters for new sign up fields
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(:email, :password, :password_confirmation, :firstname, :lastname) }
+      
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(:email, :password, :password_confirmation, :current_password, :firstname, :lastname) }
+    end
 end
