f436214ad9e8268b7448c9ecbc86c2b9089a1578
morihito <bartek.lesniak86@gmail.com>
2015-09-18 16:46:40 +0000

    Updated products controller
    Authenticate user before action
    Added correct user check, only Product owner can do edit and update actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_action :correct_user, only: [:edit, :update]
+  
   expose(:category)
   expose(:products)
   expose(:product)
@@ -46,5 +49,12 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+  
+  def correct_user
+    unless self.product.user == current_user
+      redirect_to category_product_url(category, product),
+      flash: { error: 'You are not allowed to edit this product.'}
+    end
   end
 end
