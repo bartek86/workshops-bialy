dadae2b8b4c864ea93d0864e82247f1511eb6399
morihito <bartek.lesniak86@gmail.com>
2015-09-18 17:31:15 +0000

    Added seed data
    Seed data created with Faker:
    5 users, admin, 3 categories, 30 products and 300 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# User seed
+
+5.times do
+    User.create(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(8),
+        firstname: Faker::Name.first_name,
+        lastname: Faker::Name.last_name
+    )
+end
+
+
+# admin seed
+
+User.create(
+    email: 'admin@example.com',
+    password: 'admin123',
+    firstname: 'The',
+    lastname: 'Admin',
+    admin: true)
+
+
+# Category seed    
+3.times do
+   Category.create(
+       name: Faker::Product.brand
+    )
+end
+
+# Product seed
+30.times do
+    Product.create(
+        title: Faker::Product.product,
+        description: Faker::Lorem.paragraph,
+        price: rand(1..100) + rand.round(2),
+        category_id: rand(1..3),
+        user_id: rand(1..5)
+    )
+end
+
+# Review seed
+300.times do
+    Review.create(
+        content: Faker::Lorem.paragraph,
+        rating: rand(1..5),
+        product_id: rand(1..30),
+        user_id: rand(1..5)
+        )
+end 
