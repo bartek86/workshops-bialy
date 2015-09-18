7f09b525f3604cace9365820afcf3a48b9ae140a
morihito <bartek.lesniak86@gmail.com>
2015-09-18 16:26:58 +0000

    Updated Product model
    Added presence validations and price format validation
    Added avaerage_rating method if Product has 1 or more reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :price
+  
+  validates :price, 
+            :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+            
+  def average_rating
+    return self.reviews.average(:rating).round(1) if self.reviews.count > 0
+  end
+  
 end
